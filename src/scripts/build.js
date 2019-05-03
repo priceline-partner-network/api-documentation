e8a1951ed4043edd88fea1f9d3c3ba3066da6bcc
Ryan Kim <ryan.kim@priceline.com>
2019-05-03 12:04:16 -0500

    scrapper working.

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -5,15 +5,36 @@ import logger from './logger';
 import config from './../../webpack.config';
 
 const PROJECT_ROOT = `${process.env.PWD}/src`;
+const HERODOTUS_CONFIG_JS = 'herodotus.config.js';
+const HERODOTUS_CONFIG_JSON = 'herodotus.config.json';
+
+const HERODOTUS_CONFIG_NAMES = [
+    HERODOTUS_CONFIG_JS,
+    HERODOTUS_CONFIG_JSON
+];
 
 const build = () => {
-    const modules = {};
+    const configPaths = getConfigs();
 
-    fs.readdir(PROJECT_ROOT, (err, files) => {
-        files.forEach(file => {
+    console.log('configs', configPaths);
+};
 
-        });
-    })
+const getConfigs = (directory = PROJECT_ROOT) => {
+    let configs = [];
+    const files = fs.readdirSync(directory);
+
+    files.forEach(file => {
+        const filePath = `${directory}/${file}`;
+        const stats = fs.statSync(filePath);
+
+        if(stats.isDirectory()) {
+            configs = configs.concat(getConfigs(filePath));
+        } else if(stats.isFile() && HERODOTUS_CONFIG_NAMES.includes(file)) {
+            configs.push(filePath);
+        }
+    });
+
+    return configs;
 };
 
 build();
--- src/updates/herodotus.config.js
+++ src/updates/herodotus.config.js
@@ -2,36 +2,45 @@ import updates from './updates.md';
 import sandbox from './sandbox.md';
 
 module.exports = {
-    0: {
-        title: "Launch Documentation Version 2",
-        created: "",
-        updated: "",
-        markdown: updates,
-        tags: [
-            "documentation"
-        ]
+    title: "",
+    component: "",
+    pagination: {
+        limit: 10,
+        orderBy: "latest-created"
     },
-    1: {
-        title: "API Sandbox Launch",
-        created: "",
-        updated: "",
-        markdown: sandbox,
-        tags: [
-            "sandbox"
-        ]
-    },
-    2: {
-        title: "Hardcoded document new",
-        created: "",
-        updated: "",
-        markdown: "asdfasdfasdf",
-        tags: [
-            "asdfasdfasdf",
-            "asdfasdfasdf"
-        ]
-    },
-    3: {
-        title: "",
+    content: {
+        0: {
+            title: "Launch Documentation Version 2",
+            created: "",
+            updated: "",
+            markdown: updates,
+            author: "",
+            tags: [
+                "documentation"
+            ],
+        },
+        1: {
+            title: "API Sandbox Launch",
+            created: "",
+            updated: "",
+            markdown: sandbox,
+            tags: [
+                "sandbox"
+            ]
+        },
+        2: {
+            title: "Hardcoded document new",
+            created: "",
+            updated: "",
+            markdown: "asdfasdfasdf",
+            tags: [
+                "asdfasdfasdf",
+                "asdfasdfasdf"
+            ]
+        },
+        3: {
+            title: "",
 
+        }
     }
 };
